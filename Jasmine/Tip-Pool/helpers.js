ce9a54ef4695a91efb423c6506f1c33455facf04
KTenpa <152925377+KTenpa@users.noreply.github.com>
2024-05-06 21:21:23 -0500

    Add files via upload

---
+++ Jasmine/Tip-Pool/helpers.js
@@ -0,0 +1,48 @@
+
+// accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
+function sumPaymentTotal(type) {
+  let total = 0;
+
+  for (let key in allPayments) {
+    let payment = allPayments[key];
+
+    total += Number(payment[type]);
+  }
+
+  return total;
+}
+
+// converts the bill and tip amount into a tip percent
+function calculateTipPercent(billAmt, tipAmt) {
+  return Math.round(100 / (billAmt / tipAmt));
+}
+
+// expects a table row element, appends a newly created td element from the value
+function appendTd(tr, value) {
+  let newTd = document.createElement('td');
+  newTd.innerText = value;
+
+  tr.append(newTd);
+}
+
+// append delete button and click handler for removing server from allServers and DOM td
+function appendDeleteBtn(tr, type) {
+  let newTd = document.createElement('td');
+  newTd.className = 'deleteBtn';
+  newTd.innerText = 'X';
+
+  newTd.addEventListener('click', removeEle);
+
+  tr.append(newTd);
+}
+
+function removeEle(evt) {
+  /* in this case does closest method search for a tr at current element and 
+  if not found moves to the parent element?*/
+  let ele = evt.target.closest('tr');
+
+  delete allServers[ele.id];
+ 
+  ele.parentNode.removeChild(ele);
+  updateServerTable();
+}
